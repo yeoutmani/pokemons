91e63e9aeb84d733a70e48b2ca613f0560f09ee7
yeoutmani <ysn.outmani@gmail.com>
2022-06-22 02:25:41 +0100

    Request with saga

--- src/redux/content/action.js
+++ src/redux/content/action.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import store from "../store";
+/** @format */
+
 import ContentTypes from "./contentType";
 
 export const fetchContentStart = () => ({
@@ -8,43 +8,20 @@ export const fetchContentStart = () => ({
 
 export const fetchContentSuccess = (ContentData) => ({
   type: ContentTypes.FETCH_CONTENT_SUCCESS,
-  payload : ContentData
-});
-export const fetchContentErreur = (error) => ({
-  type: ContentTypes.FETCH_CONTENT_ERREUR,
-  payload : error
+  payload: ContentData,
 });
 
+export const fetchContentErreur = (error) => ({
+  type: ContentTypes.FETCH_CONTENT_ERREUR,
+  payload: error,
+});
 
 export const setModaleVisible = (isModalVisible) => ({
   type: ContentTypes.SET_MODAL_VISIBLE,
-  payload : isModalVisible
+  payload: isModalVisible,
 });
 
 export const setContentVar = (contentVar) => ({
   type: ContentTypes.SET_CONTENT_VAR,
-  payload : contentVar
+  payload: contentVar,
 });
-
-export const setContentData = (contentData) => ({
-  type: ContentTypes.SET_CONTENT_DATA,
-  payload : contentData
-});
-
-/*
-  export function fetchContentStartAsync(dispatch) {
-   let state = store.getState().ContentReducer;
-   let url = state.contentVar.url;
-
-    return function(dispatch) {
-      return axios.get(url)
-        .then(({ data }) => {
-          dispatch(setContentData(data));
-          dispatch(fetchContentSuccess(data));
-
-      /*  dispatch(fetchPokemonsSuccess(results));
-        dispatch(setNextUrl(data.next));
-        //dispatch(setNextUrl(data.next));*/
- /*     }).catch(error => dispatch(fetchContentErreur(error)))
-    };
-  }*/
--- src/redux/content/content.js
+++ src/redux/content/content.js
@@ -1,48 +1,47 @@
+/** @format */
+
+import ContentTypes from "./contentType";
+
 const INITIAL_STATE = {
-    contentData : null,
-    contentVar : {title :''},
-    isModalVisible : false,
-    isFetching:false,
-    errorMessage : undefined,
-  };
-  
-  const contentReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-      case "FETCH_CONTENT_START":
-        return {
-          ...state,
-          isFetching: true,
-        };
-      case "FETCH_CONTENT_SUCCESS":
-        return {
-          ...state,
-          isFetching: false,
-          contentData : action.payload
-        };
-      case "FETCH_CONTENT_ERREUR":
-          return {
-            ...state,
-            isFetching: false,
-            errorMessage : action.payload
-          };
-      case "SET_MODAL_VISIBLE":
-            return {
-                ...state,
-                isModalVisible: action.payload,
-              };
-       case "SET_CONTENT_VAR":
-                return {
-                    ...state,
-                    contentVar: action.payload,
-                  };
-                  case "SET_CONTENT_DATA":
-                    return {
-                        ...state,
-                        contentData: action.payload,
-                      };
-      default:
-        return state;
-    }
-  };
-  
-  export default contentReducer;
+  contentData: null,
+  contentVar: { title: "" },
+  isModalVisible: false,
+  isFetching: false,
+  errorMessage: undefined,
+};
+
+const contentReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case ContentTypes.FETCH_CONTENT_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case ContentTypes.FETCH_CONTENT_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        contentData: action.payload,
+      };
+    case ContentTypes.FETCH_CONTENT_ERREUR:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+      };
+    case ContentTypes.SET_MODAL_VISIBLE:
+      return {
+        ...state,
+        isModalVisible: action.payload,
+      };
+    case ContentTypes.SET_CONTENT_VAR:
+      return {
+        ...state,
+        contentVar: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default contentReducer;
